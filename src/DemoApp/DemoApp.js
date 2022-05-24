8906a43167b9bf05463ae00ad264abe3fbbb591a
OrinVukelic <orin.vukelic@gmail.com>
2022-05-24 16:58:35 +0200

    Added description and optimized

--- src/DemoApp/DemoApp.js
+++ src/DemoApp/DemoApp.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import './DemoApp.scss';
 import Tool from './Tool';
 import ToolFrame from './ToolFrame';
-import chatimg from './chatimg.png'
-import cconvertimg from './cconvertimg.png'
-import drawimg from './drawimg.png'
+import chatimg from './chatimg.webp'
+import cconvertimg from './cconvertimg.webp'
+import drawimg from './drawimg.webp'
 
 function DemoApp() {
 
@@ -21,6 +21,23 @@ function DemoApp() {
         <h1>Demo project</h1>
       </div>
       <br />
+      <div id="description" className="container">
+        <p className="ind">Welcome! This is a simple demonstrational application which presents a few ways to use web development technologies. 
+        Languages used to make this website include:</p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>PHP</li>
+          <li>SQL</li>
+        </ul>
+        <p>with the addition of:</p>
+        <ul>
+          <li>Sass</li>
+          <li>Bootstrap</li>
+          <li>ReactJS</li>
+        </ul>
+      </div>
       <div id="select" className="container">
         <h3>Choose one of the tools:</h3>
         <div className="row w-75 mx-auto">
--- src/DemoApp/Tool.js
+++ src/DemoApp/Tool.js
@@ -4,7 +4,7 @@ function Tool({name, src, onClick}) {
     <div className="col-md-4 tool">
       <button className="tbutton h-100" onClick={onClick}>
         <h3>{name}</h3>
-        <img src={src} alt={name} className="img-fluid" />
+        <img src={src} alt={name} className="img-fluid" width="400px" height="400px" />
       </button>
     </div>
   );
